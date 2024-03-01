2b8c7e4b89f6f34c2508db67945a9e078b3d9450
Thomas Kim <tkimm156@gmail.com>
2024-03-01 14:32:07 -0600

    Work on form html and validation

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -16,7 +17,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", "postgresql:///adopt")
 
-debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
@@ -29,5 +29,23 @@ toolbar = DebugToolbarExtension(app)
 
 @app.get('/')
 def show_homepage():
-    pets = Pet.query.all()
+    # pets = Pet.query.all()
     return render_template('home.html', pets=pets)
+
+
+@app.route("/add", method=["POST", "GET"])
+def add_pet():
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        # name = form.name.data
+        # species = form.species.data
+        # photo_url = form.photo_url.data
+        # age = form.age.data
+        # notes = form.notes.data
+        # available = form.available.data
+        return redirect("/")
+
+    else:
+        return render_template("add_pet_form.html", form=form)
--- forms.py
+++ forms.py
@@ -1,1 +1,28 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, BooleanField, URLField
+from wtforms.validators import InputRequired, Optional, URL
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets"""
+
+    name = StringField("Pet Name",
+                       validators=[InputRequired()])
+
+    species = StringField("Pet Species",
+                          validators=[InputRequired()])
+
+    photo_url = URLField("Pet Photo URL",
+                         validators=[Optional(), URL()])
+
+    age = SelectField("Pet Age",
+                      choices=[("baby", "Baby"), ("young", "Young"),
+                               ("adult", "Adult"), ("senior", "Senior")])
+
+    notes = StringField("Pet Notes")
+
+    available = BooleanField("Pet Availability",
+                             validators=[InputRequired()],
+                             coerce=bool)
+
